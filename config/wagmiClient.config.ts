487cd68eac9ea8acba27ddfab82070ed89db95aa
april <hesiyuetian@outlook.com>
2025-04-25 11:45:03 +0800

    Upgrade 'viem' to version 2.28.0 and 'wagmi' to version 2.15.0 in package.json. Update pnpm-lock.yaml to reflect these changes. Refactor wagmiClient.config.ts to define custom chains with specific RPC URLs for mainnet and BSC.

--- config/wagmiClient.config.ts
+++ config/wagmiClient.config.ts
@@ -1,9 +1,26 @@
 import { type Chain, connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { coinbaseWallet, metaMaskWallet, okxWallet, rabbyWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
+import { createClient, defineChain } from 'viem';
 import { type Config, createConfig, http, createStorage, cookieStorage } from 'wagmi';
 import { mainnet, bsc, bscTestnet, sepolia } from 'wagmi/chains';
 
-export const chains: any = ['production', 'preview'].includes(process.env.APP_ENV!) ? ([mainnet, bsc] as Chain[]) : ([sepolia, bscTestnet] as Chain[]);
+const customBsc = defineChain({
+    ...bsc,
+    rpcUrls: {
+        default: { http: ['https://bsc-mainnet.infura.io/v3/fb40925bfc8444b0bc5677870300725c', 'https://bsc.blockrazor.xyz', 'https://bsc-dataseed1.defibit.io'] }
+    }
+});
+
+const customMainnet = defineChain({
+    ...mainnet,
+    rpcUrls: {
+        default: {
+            http: ['https://mainnet.infura.io/v3/fb40925bfc8444b0bc5677870300725c', 'https://rpc.mevblocker.io']
+        }
+    }
+});
+
+export const chains: [Chain, ...Chain[]] = ['production', 'preview'].includes(process.env.APP_ENV!) ? [customMainnet, customBsc] : [sepolia, bscTestnet];
 // export const chains = [mainnet, bsc, sepolia, bscTestnet] as const satisfies Chain[];
 
 export const connectors = connectorsForWallets(
@@ -26,11 +43,8 @@ export const config = createConfig({
     connectors,
     ssr: true,
     multiInjectedProviderDiscovery: true,
-    transports: {
-        [mainnet.id]: http(),
-        [bsc.id]: http(),
-        [sepolia.id]: http(),
-        [bscTestnet.id]: http()
+    client({ chain }: any) {
+        return createClient({ chain, transport: http() });
     }
     // storage: createStorage({
     //     storage: cookieStorage
